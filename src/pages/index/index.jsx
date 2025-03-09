9f44a79ac12624e56e093a3bbd4961555e1dc9df
Mega Nugraha <monmega110@gmail.com>
2025-03-09 22:34:56 +0700

    updating imports

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,4 +1,4 @@
-import Header from "../components/Header";
+import Header from "../../components/Header";
 import UserInfo from "./components/UserInfo";
 import { useNavigate } from "react-router-dom";
 
